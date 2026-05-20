ef58002d542a8492207a50d05192027a2e47a6ea
Michel Baradosa <nyamoga9@gmail.com>
2026-05-20 12:05:55 -0400

    Add DebtOps app screen logo

--- debtops/hooks.py
+++ debtops/hooks.py
@@ -5,3 +5,15 @@ app_description = "Manage company debt facilities, amortization schedules, and E
 app_email = ""
 app_license = "mit"
 required_apps = ["erpnext"]
+
+app_logo_url = "/assets/debtops/images/debtops.svg"
+app_home = "/app/debtops"
+
+add_to_apps_screen = [
+    {
+        "name": app_name,
+        "logo": app_logo_url,
+        "title": app_title,
+        "route": app_home,
+    }
+]
